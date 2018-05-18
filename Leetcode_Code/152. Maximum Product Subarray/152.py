026b7f88e4b57fb1a4f1d9befeb387b80646d24d
Robert Bogan Kang <bob.kang@qq.com>
2018-05-18 14:59:11 -0500

    Update 152.py

--- Leetcode_Code/152. Maximum Product Subarray/152.py
+++ Leetcode_Code/152. Maximum Product Subarray/152.py
@@ -6,7 +6,6 @@ class Solution:
         """
         max_num = curmin = curmax = nums[0]
         for i in range(1, len(nums)):
-            print(max_num)
             cur = nums[i]
             curmax, curmin = max(curmax * cur, curmin * cur, cur), min(curmax * cur, curmin * cur, cur)
             max_num = max(curmax, max_num)
