8b5797af18996f23f0b58ea9c25b86b0989edc66
Robert Bogan Kang <bob.kang@qq.com>
2018-05-18 12:46:49 -0500

    Create 98.py

---
+++ Leetcode_Code/98. Validate Binary Search Tree/98.py
@@ -0,0 +1,34 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+
+    def isValidBST(self, root):
+        pre = 0
+        i = 0
+        result = True
+
+        def dfs(node):
+            nonlocal i, pre, result
+            if not result:
+                if not node:
+                    return
+                dfs(node.left)
+                if i != 0 and pre >= node.val:
+                    result = False
+                i += 1
+                pre = node.val
+                dfs(node.right)
+
+        dfs(root)
+        return result
