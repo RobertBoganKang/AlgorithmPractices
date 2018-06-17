af947d13c3bd6c7aadcac010366c64857c3591a5
Robert Bogan Kang <bob.kang@qq.com>
2018-06-16 21:46:45 -0500

    Create 840_wrong_answer.py

---
+++ Leetcode_Code/840. Magic Squares In Grid/840_wrong_answer.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numMagicSquaresInside(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        def check_magic_matrix(idx, idy):
+            # first row sum as target
+            target = grid[idx][idy] + grid[idx][idy + 1] + grid[idx][idy + 2]
+            # check rows
+            for i in range(1, 3):
+                total = 0
+                for j in range(3):
+                    total += grid[idx + i][idy + j]
+                if total != target:
+                    return False
+            # check columns
+            for j in range(3):
+                total = 0
+                for i in range(3):
+                    total += grid[idx + j][idy + i]
+                if total != target:
+                    return False
+            # check diagonals
+            if grid[idx][idy] + grid[idx + 1][idy + 1] + grid[idx + 2][idy + 2] != target \
+                    or grid[idx][idy + 2] + grid[idx + 1][idy + 1] + grid[idx + 2][idy] != target:
+                return False
+            return True
+
+        result = 0
+        for I in range(len(grid) - 2):
+            for J in range(len(grid[0]) - 2):
+                if check_magic_matrix(I, J):
+                    result += 1
+        return result
+
