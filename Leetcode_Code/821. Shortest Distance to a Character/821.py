9efb435d1e26248c5b178f4a7fb6d8beb46b8a35
Robert Bogan Kang <bob.kang@qq.com>
2018-06-17 00:27:34 -0500

    Create 821.py

---
+++ Leetcode_Code/821. Shortest Distance to a Character/821.py
@@ -0,0 +1,43 @@
+class Solution:
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        if S is None or C is None or S == "":
+            return []
+        #
+        result = [0 for _ in range(len(S))]
+        # detect first letter
+        i = 0
+        last = 0
+        count = 0
+        # loop second or more
+        while i < len(S):
+            num = 1
+            while i < len(S) and S[i] != C:
+                result[i] = num
+                num += 1
+                i += 1
+            if count == 0:
+                back_num = 0
+                if i >= len(S):
+                    return [0 for _ in range(len(S))]
+            else:
+                back_num = num // 2
+            # backtrace
+            j = i
+            num = 0
+            while len(S) > j >= last + back_num:
+                result[j] = num
+                num += 1
+                j -= 1
+            count += 1
+            i += 1
+            last = i
+        return result
+
+
+s = Solution()
+print(s.shortestToChar("aaba", "b"))
